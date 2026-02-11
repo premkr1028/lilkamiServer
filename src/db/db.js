a8180eaa4b82e9f66bc686de2642a8ab7b780999
Prem Kumar Balmiki <premkumarbalmiki987@gmail.com>
2026-02-11 22:08:02 +0530

    db

--- src/db/db.js
+++ src/db/db.js
@@ -1,8 +1,24 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-async function connectDb() {
-    await mongoose.connect("mongodb+srv://prem:anipaper2810@cluster0.ytly8wx.mongodb.net/lilkami")
-    console.log("db connected")
-}
+// async function connectDb() {
+//     await mongoose.connect("mongodb+srv://prem:anipaper2810@cluster0.ytly8wx.mongodb.net/lilkami")
+//     console.log("db connected")
+// }
 
-export default connectDb
+// export default connectDb
+
+// db.js or similar
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) return;
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("Connection error:", error);
+  }
+};
+
+export default connectDB;
